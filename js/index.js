26dd1310dfd656655ed2caa662838cdaf392fc6a
Amaya Davis <aedavis2005@gmail.com>
2026-06-02 22:59:16 -0500

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -16,3 +16,29 @@ for(let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+fetch("https://api.github.com/users/giddydavis1020/repos")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    const repositories = data;
+    console.log(repositories);
+
+    const projectSection = document.getElementById("projects");
+    const projectList = projectSection.querySelector("ul");
+
+    for (let i = 0; i < repositories.length; i++) {
+
+    const project = document.createElement("li"); 
+
+    project.innerText = repositories[i].name;
+
+    projectList.appendChild(project);
+}
+
+})
+.catch(function(error) {
+    console.error("Failed to load repositories:", error);
+});
+
